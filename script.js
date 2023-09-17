546c1cd514361111a516d502c4e76e3ad03e2eaa
lucas fernandes guanabara <lucasff.guanabara@gmail.com>
2023-09-16 21:53:52 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,110 @@
+// API APOD + BOTOES 
+
+const apiKey = "vdu70esjaX6AvxMuTIa4UnmHFVY261gUE5AqlmDV";
+const url = "https://api.nasa.gov/planetary/apod?";
+
+let container = document.querySelector("main");
+let button = document.getElementById("fetch-image");
+
+button.addEventListener("click", () => {
+  getImage("normal");
+});
+
+let hdButton = document.getElementById("fetch-hd");
+hdButton.addEventListener("click", () => {
+  getImage("hd");
+});
+
+function getImage(value) {
+  let imageContainer = document.querySelector(".image-container");
+  imageContainer.remove();
+
+  let newImageContainer = document.createElement("div");
+  newImageContainer.classList.add("image-container");
+
+  container.append(newImageContainer);
+
+  let dateInput = document.querySelector(".details-input input");
+  let date = dateInput.value;
+
+  let request = new XMLHttpRequest();
+  request.open("GET", url + "date=" + date + "&api_key=" + apiKey, true);
+  request.send();
+  request.onload = function () {
+    if (request.status === 200) {
+      let data = JSON.parse(request.responseText);
+      let imageUrl;
+      if (value === "hd") {
+        imageUrl = data.hdurl;
+      } else {
+        imageUrl = data.url;
+      }
+      let image = document.createElement("img");
+      image.src = imageUrl;
+      newImageContainer.append(image);
+    } else {
+      window.alert("Coloque um formato de data correto! Ex: (2023-09-16).");
+    }
+  };
+}
+
+
+// Botao auto scroll down home
+
+const botao = document.querySelector('#botao-arrow')
+
+botao.addEventListener('click', () => {
+  window.scroll({top: window.innerHeight, behavior: "smooth"})
+  botao.style.display = 'none'
+});
+
+
+
+
+
+
+
+
+
+// ABAIXO SALVANDO ALGUNS CODIGOS QUE DERAM CERTO, MAS NAO DA FORMA QUE EU QUERIA, DEIXAREI O COMENTARIO DELES AQUI PRA QUEM SABE USAR NO FUTURO!!!!
+
+// ------------------------------------------------------------------------------------>
+
+// FADE DE ROLAGEM
+
+/* let sections = document.querySelectorAll("#services");
+window.onscroll = function fadeIn() {
+    sections.forEach(section => {
+        let sectionSize = section.getBoundingClientRect();
+        let bottomPart = sectionSize.bottom;
+        if (window.scrollY >= bottomPart) {
+            section.style.opacity = "1";
+            section.style.transform = "translateX(0)";
+            section.style.transition = "1s ease-in-out";
+        } else {
+            section.style.opacity = "0";
+            section.style.transform = "translateX(-10px)";
+            section.style.transition = "1s ease-in-out";
+        }
+    });
+} */
+
+
+// --------------------------------------------------------------------------------->
+
+// outra tentativa de scroll
+
+// $(function() {
+/*  $('.scroll-down').click (function() {
+    $('html, body').animate({scrollTop: $('section.ok').offset().top }, 'slow');
+    return false;
+  });
+ }); */
+
+
+
+
+
+
+
+
